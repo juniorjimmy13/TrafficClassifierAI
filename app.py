9cbdbd830c1c5507fcb2e6b1140b43f3972c20a9
jimmyjr <juniorjimmy13@gmail.com>
2025-04-09 20:19:25 +0300

    container

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def preprocess_image(image: Image.Image):
 # Prediction logic
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     with st.spinner("🔍 Analyzing image..."):
         
